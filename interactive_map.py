d5964da7a96f8403b988273935315f678e301337
qsalome <qsalome.astro@gmail.com>
2025-07-25 15:50:50 +0300

    Format munipalities popup

--- interactive_map.py
+++ interactive_map.py
@@ -3,6 +3,12 @@ import folium
 from folium.plugins import GroupedLayerControl
 import base64
 
+from geocube.vector import vectorize
+
+import branca
+from branca.element import MacroElement
+from jinja2 import Template
+
 import argparse
 import pandas as pd
 import geopandas
@@ -21,16 +27,33 @@ import matplotlib.cm as cm
 from folium import IFrame
 
 
-def get_color(x):
-    decimals = 2
-    x = np.around(x, decimals=decimals)
-    ls = np.linspace(0,1,10**decimals+1)
-    if x==-9999:
-        return (0, 0, 0, 0)
-    elif x != -9999:
-        return cm.get_cmap('terrain')(ls)[np.argwhere(ls==x)]
-    else:
-        raise ValueError()
+#--------------------------------------------------------------------
+class BindColormap(MacroElement):
+    """Binds a colormap to a given layer.
+
+    Parameters
+    ----------
+    colormap : branca.colormap.ColorMap
+        The colormap to bind.
+    """
+    def __init__(self, layer, colormap):
+        super(BindColormap, self).__init__()
+        self.layer = layer
+        self.colormap = colormap
+        self._template = Template(u"""
+        {% macro script(this, kwargs) %}
+            {{this.colormap.get_name()}}.svg[0][0].style.display = 'block';
+            {{this._parent.get_name()}}.on('overlayadd', function (eventLayer) {
+                if (eventLayer.layer == {{this.layer.get_name()}}) {
+                    {{this.colormap.get_name()}}.svg[0][0].style.display = 'block';
+                }});
+            {{this._parent.get_name()}}.on('overlayremove', function (eventLayer) {
+                if (eventLayer.layer == {{this.layer.get_name()}}) {
+                    {{this.colormap.get_name()}}.svg[0][0].style.display = 'none';
+                }});
+        {% endmacro %}
+        """)
+
 #--------------------------------------------------------------------
 def day_range(year=2024,month=1,day=None):
    """
@@ -117,6 +140,98 @@ def extract_temperatures(municipalities,quantity="mean temperature",
    return muni_temp,temp_avg
 
 #--------------------------------------------------------------------
+def new_image_layer(raster,interactive_map,name="",visible=False,
+         year=2024,month=1,day=None):
+   raster = raster.rio.reproject("EPSG:4326")
+   gdf    = vectorize(raster.astype("float32"))
+   gdf["id"] = gdf.index.astype(str)
+   boundList = [x for x in raster.rio.bounds()]
+   bounds=[[boundList[1],boundList[0]], [boundList[3],boundList[2]]]
+
+   minmax = float(raster.min().values),float(raster.max().values)
+   print(minmax)
+   raster.attrs['_FillValue']=-9999
+   raster = raster.fillna(value=-9999)
+
+#   layer = folium.raster_layers.ImageOverlay(
+#         image=raster.values,
+#         bounds=[[boundList[1],boundList[0]], [boundList[3],boundList[2]]],
+#         opacity=0.6,
+#         name=name,
+#         colormap=cm.terrain,
+#         vmin=minmax[0], vmax=minmax[1],#lambda x: get_color(x)
+#         show=visible
+#   )
+
+   layer = folium.Choropleth(
+         geo_data=gdf,
+         data=gdf,
+         columns=("id","_data"),
+         key_on="feature.id",
+
+         bins=9,
+         fill_color="YlOrRd",
+         line_weight=0,
+         opacity=0.6,
+         legend_name="Mean temperature",
+         name=name,
+         show=visible,
+
+#         highlight=True
+   )
+
+   return layer
+#--------------------------------------------------------------------
+def new_polygon_layer(gdf,name="",image=None,
+         year=2024,month=1,day=None):
+
+   if image is not None:
+      popup_html = np.array([])
+      for idx in range(len(gdf)):
+         city = gdf["NAMEFIN"][idx]
+         imname  = image.name.format(f"{month_name[month]}",city)
+         encoded = base64.b64encode(
+                     open(image.with_name(imname), 'rb').read()).decode()
+         string = f'<img src="data:image/png;base64,{encoded}" width="300">'
+         popup_html = np.append(popup_html,string)
+      gdf['popup_html'] = popup_html
+
+      popup = folium.GeoJsonPopup(
+         fields=["popup_html"],
+         aliases=[""],
+         labels=True,
+         sticky=False,
+         localize=True,
+      )
+   else:
+      popup=None
+
+   # Define custom tooltip with HTML
+   tooltip = folium.features.GeoJsonTooltip(
+      fields=("NAMEFIN",f"TEMP_EVO_{month_name[month]}",),
+      aliases=("Municipality:","1961-2024 (°C):",),
+      labels=True,
+      sticky=False,
+      localize=True,
+      )
+
+   layer = folium.GeoJson(
+      gdf,
+      style_function=lambda feature: {
+            "fillColor": "transparent",
+            "color": "black",
+            "weight": 2
+            },
+      opacity=0.8,
+      name=name,
+      show=False,
+      tooltip=tooltip,
+      popup=popup
+   )
+
+   return layer
+
+#--------------------------------------------------------------------
 
 
 
@@ -136,180 +251,64 @@ interactive_map = folium.Map(
     max_lat=70.1,
     min_lon=21.2,
     max_lon=31.6,
-    interactive=False
+    interactive=True
+)
+
+
+YlGn_cmap = branca.colormap.LinearColormap(
+    colors=branca.colormap.linear.YlGn_09.colors,
+    vmin=0,
+    vmax=state_data.Unemployment.max(),  # setting max value for legend
+    caption='Unemployment Rate (%)'
+)
+
+
+YlGn_cmap = branca.colormap.LinearColormap(
+    colors=branca.colormap.linear.YlGn_09.colors,
+    vmin=0,
+    vmax=state_data.Unemployment.max(),  # setting max value for legend
+    caption='Unemployment Rate (%)'
 )
 
 
 # Read the Finland municipalities (as defined in 2021)
 municipalities = geopandas.read_file(DATA_DIRECTORY /
-         "finland_municipalities_2021.gpkg").to_crs("EPSG:3067")
-         #.to_crs("EPSG:4326")
+         "finland_municipalities_temperature_evolution.gpkg"
+         ).to_crs("EPSG:3067")
 
-
-# Select the municipalities of interest
-cities = ["Helsinki", "Jyväskylä", "Oulu", "Rovaniemi", "Inari"]
-
-indexes = [i for i in municipalities.index 
-               if municipalities["NAMEFIN"][i] in cities]
-municipalities = municipalities.iloc[indexes]
-
-images = np.array([])
-for city in municipalities['NAMEFIN']:
-   images = np.append(images,city)
 
 temp_layers = {}
 muni_layers = {}
-k=0
+k1,k2   = 0,0
+visible = True
 
-
-svg = """
-<object data="data:image/jpg;base64,{}" width="{}" height="{} type="image/svg+xml">
-</object>""".format
-
-width, height, fat_wh = 200, 140, 1.3
-
-
-for year in tqdm([1961,1981,2001,2021]):
-   for month in [1,7]:
+for month in [1,7]:
+   for year in tqdm([1961,1981,2001,2021]):
       mean_temp,map = extract_temperatures(municipalities,"mean temperature",
                         year=year,month=month)
 
-      map = map.rio.reproject("EPSG:4326")
-      minmax = float(map.min().values),float(map.max().values)
-      map.attrs['_FillValue']=-9999
-      map = map.fillna(value=-9999)
 
-      if((year == 1961) & (month == 1)):
-         visible = True
-      else:
-         visible = False
+      folium_layer = new_image_layer(map,interactive_map.crs,
+               f"{month_name[month]} {year}",
+               visible,year,month)
+      temp_layers.update({f"{k1}": folium_layer})
+      temp_layers[f"{k1}"].add_to(interactive_map)
 
-      boundList = [x for x in map.rio.bounds()]
-      temp_layers.update({f"{k}": 
-          folium.raster_layers.ImageOverlay(
-             image=map.values,
-             bounds=[[boundList[1],boundList[0]], [boundList[3],boundList[2]]],
-             opacity=0.6,
-             name=f"{month_name[month]} {year}",
-             colormap=cm.terrain,
-             vmin=minmax[0], vmax=minmax[1],#lambda x: get_color(x)
-             show=visible
-          )
-      })
-      temp_layers[f"{k}"].add_to(interactive_map)
+      visible = False
+      k1 = k1+1
 
 
-      municipalities["temperature"] = (100*mean_temp).astype('int')/100
-#      municipalities["image_url"] = "figures/test.jpg"
+   imname = FIG_DIRECTORY / "Temperatures_evolution"
+   imname = imname / "Temperature_{}_{}.png"
+   folium_layer = new_polygon_layer(municipalities,
+               f"{month_name[month]}",image=imname,
+               year=year,month=month)
+   muni_layers.update({f"{k2}": folium_layer})
+   muni_layers[f"{k2}"].add_to(interactive_map)
+
+   k2 = k2+1
 
 
-
-#      popup  = folium.Popup(iframe, parse_html = True, max_width=1500)
-
-      popup_html = np.array([])
-      for idx in range(len(municipalities)):
-         encoded = base64.b64encode(open("figures/test.png", 'rb').read()).decode()
-#         iframe = IFrame(svg(encoded.decode('UTF-8'), width, height) , width=width*fat_wh, height=height*fat_wh)
-         string = f'<img src="data:image/png;base64,{encoded}" width="300">'
-
-#         municipality = municipalities.iloc[idx]
-#         string = f"""<img src="{municipality['image_url']}"
-#                  width="200" height="142">
-#               """
-         popup_html = np.append(popup_html,string)
-      municipalities['popup_html'] = popup_html
-
-      popup = folium.GeoJsonPopup(
-         fields=["popup_html"],
-         aliases=[""],
-         labels=True,
-         sticky=False,
-         localize=True,
-      )
-
-      # Define custom tooltip with HTML
-      tooltip = folium.features.GeoJsonTooltip(
-         fields=("NAMEFIN","temperature",),
-         aliases=("Municipality:","Mean temperature:",),
-#         labels=True,
-#         sticky=True,
-         localize=True,
-         )
-#      tooltip = folium.features.GeoJsonTooltip(
-#         fields=("tooltip_html",),
-##         aliases=(""),
-##         labels=True,
-##         sticky=True,
-#         localize=True,
-#         )
-
-      muni_layers.update({f"{k}":
-         folium.GeoJson(
-            municipalities,
-            style_function=lambda feature: {
-                  "fillColor": "transparent",
-                  "color": "black",
-                  "weight": 2
-                  },
-            opacity=0.8,
-            name=f"{month_name[month]} {year}",
-            show=False,
-            tooltip=tooltip,
-            popup=popup
-         )
-      })
-
-      
-      muni_layers[f"{k}"].add_to(interactive_map)
-      k = k+1
-
-#interactive_map
-
-## Read the Finland municipalities (as defined in 2021)
-#municipalities = geopandas.read_file(DATA_DIRECTORY /
-#         "finland_municipalities_2021.gpkg").to_crs("EPSG:3067")
-#         #.to_crs("EPSG:4326")
-
-
-## Select the municipalities of interest
-#cities = ["Helsinki", "Jyväskylä", "Oulu", "Rovaniemi", "Inari"]
-
-#indexes = [i for i in municipalities.index 
-#               if municipalities["NAMEFIN"][i] in cities]
-#municipalities = municipalities.iloc[indexes]
-
-
-## Initialisation temperature DataFrame for the selected month
-#temperatures = pd.DataFrame(columns=["year"]+[ f"{quantity}_temp_{city}"
-#            for city,quantity in product(cities,["mean","min","max"])])
-
-
-#for year in tqdm(range(1961,2025,1)):
-#   mean_temp = extract_temperatures(municipalities,"mean temperature",
-#                     year=year,month=month)
-#   min_temp  = extract_temperatures(municipalities,"minimum temperature",
-#                     year=year,month=month)
-#   max_temp  = extract_temperatures(municipalities,"maximum temperature",
-#                     year=year,month=month)
-
-
-#   d = {"year": year}
-#   for city in cities:
-#      idx = np.where(municipalities["NAMEFIN"]==city)
-#      d[f"mean_temp_{city}"] = [float(mean_temp[idx][0])]
-#      d[f"min_temp_{city}"]  = [float(min_temp[idx][0])]
-#      d[f"max_temp_{city}"]  = [float(max_temp[idx][0])]
-
-#   new_row      = pd.DataFrame(d)
-#   temperatures = pd.concat([temperatures, new_row], ignore_index=True)
-
-
-
-#for city in cities:
-#   fig = plot_temperature_evolution(temperatures,city,
-#                  year_start=1961,year_end=2024,month=month)
-#   fig.savefig(FIG_DIRECTORY / "Temperatures_evolution" /
-#               f"Temperature_{month_name[month]}_{city}.png")
 
 folium.LayerControl(collapsed=False).add_to(interactive_map)
 
@@ -319,11 +318,11 @@ GroupedLayerControl(
    collapsed=False,
    exclusive_groups=True
 ).add_to(interactive_map)
-list_layers = [muni_layers[key] for key in temp_layers.keys()]
+list_layers = [muni_layers[key] for key in muni_layers.keys()]
 GroupedLayerControl(
    groups={'Municipalities': list_layers},
    collapsed=False,
-   exclusive_groups=False
+   exclusive_groups=True
 ).add_to(interactive_map)
 
 interactive_map.save(HTML_DIRECTORY /
